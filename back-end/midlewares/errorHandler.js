5080aeb6389b299a06cfff6444783b62eff5bf6a
Dikin19 <muhamadsodikin191919@gmail.com>
2025-10-28 22:26:21 +0700

    Handle NotFound and BadRequest in errorHandler

--- back-end/midlewares/errorHandler.js
+++ back-end/midlewares/errorHandler.js
@@ -2,17 +2,27 @@
     console.log(err, "<<< errorHandler");
 
       if (err.name === 'SequelizeValidationError' || err.name === 'SequelizeUniqueConstraintError') {
-
           // ambil semua error dengan detail message dari sequelize
           const validationErrors = err.errors.map(el => el.message);
-          
           // ambil pesan pertama dari error ini
           res.status(400).json({
             message: validationErrors[0]
           });
 
+        
+        } else if ( err.name === 'NotFound'){
+          res.status(404).json({message: err.message})
+          console.log('apakah pesan NotFound masuk  ?', err.message);
 
-        } else {
+
+        } else if (err.name === 'BadRequest'){
+          res.status(400).json({message: err.message})
+          console.log('apakah pesan BadRequest masuk  ?', err.message); 
+          
+          
+        }
+        
+        else {
           res.status(500).json({ message: 'Internal server error' });
     }
   }
